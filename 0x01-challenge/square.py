2fd6608dbeb4dfb232eb2e43c9ca11105425f46c
penscola <penscolashackletonfelix@gmail.com>
2023-10-07 19:11:05 +0300

    My square :bug: fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,77 +1,32 @@
 #!/usr/bin/python3
-"""
-Class Square Module
-"""
+"""This module contains the class square"""
 
 
-class Square():
-    """
-    Attr:
-        width
-        height
-    Methods:
-        __init__(self, *args, **kwargs)
-        area(self)
-        perimeter(self)
-        __str__(self)
-    """
-
-    width = 0
-    height = 0
+class square():
+    """Definations of the class square"""
 
     def __init__(self, *args, **kwargs):
-        """initialize square instance with width and height"""
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-            if 'width' in kwargs.keys() and self.height != self.width:
-                self.height = self.width
-            if 'height' in kwargs.keys() and self.width != self.height:
-                self.width = self.height
+        """ Initializer for square class """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            break
 
-    def area(self):
-        """ Return area of the square """
+    def area_of_my_square(self):
+        """ Area of the square """
         return self.width * self.width
 
-    def perimeter(self):
-        """ Return perimeter of the square """
-        return (self.width * 2) + (self.height * 2)
+    def permiter_of_mySquare(self):
+        """ Perimeter of the square """
+        return (self.width * 4)
 
     def __str__(self):
-        """ Return string representation """
-        return "{}/{}".format(self.width, self.height)
+        """ String format """
+        return "{}/{}".format(self.width, self.width)
 
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
-    print(s.width)
-    print(s.height)
-    print(s.area())
-    print(s.perimeter())
-    print(s.__str__())
-    print("--------------------------------")
-    q = Square(width=2)
-    print(q)
-    print(q.width)
-    print(q.height)
-    print(q.area())
-    print(q.perimeter())
-    print(q.__str__())
-    print("--------------------------------")
-    u = Square(height=3)
-    print(u)
-    print(u.width)
-    print(u.height)
-    print(u.area())
-    print(u.perimeter())
-    print(u.__str__())
-    print("--------------------------------")
-    a = Square()
-    print(a)
-    print(a.width)
-    print(a.height)
-    print(a.area())
-    print(a.perimeter())
-    print(a.__str__())
+    print(s.area_of_my_square())
+    print(s.permiter_of_mySquare())
